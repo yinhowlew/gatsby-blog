8c77faeafa139c7b9b8c220a814a4dbe8d636b88
yinhow <yinhowlew@gmail.com>
2020-05-05 13:14:39 -0400

    added posts

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 // import Header from "../components/header";
 // import styles from "../styles/about.module.css";
 import Layout from "../components/Layout";
@@ -15,16 +15,17 @@ const About = ({ data }) => {
 	    	<h3>About this blog</h3>
 		    <p>“Strong opinions, loosely held” is a framework of thinking that I aspire towards.</p>
 		    <p>I like to think through issues deeply and develop solid arguments and form firm theoretical grounds (Strong Opinions). But it is also important not to be overly attached to our thinking (Loosely Held) – especially when new information arises that challenges some of our prior assumptions. </p>	    	
-	    	<p>This blog is written primarily for myself. See here for more explanation.</p>
+	    	<p>This blog is written primarily for myself. See <Link to="hello-world" key="hello-world">here</Link> for more explanation.</p>
 	    	<h3>About me</h3>
 	    	<p>My professional journey converges on building things - tech products, startups, teams, companies.</p> 
-	    	<p>Previously at a tech consulting firm, I led the Consumer Technologies division, specialising in B2C Engagement and Loyalty. I advised clients on product strategy, and built a team of developers and designers to execute digital products.</p>
-	    	<p>Prior to that, I founded and ran an e-commerce startup in Vietnam.</p>
-	    	<p>I like to challenge myself - I'm a <a href="http://somuchpoker.com/lee-yin-how-wins-the-record-breaking-asia-poker-league-vietnam-main-event/">regional</a> and <a href="http://www.pokerkaki.com/article/the_singapore_poker_championships_ii_-_poker_in_paradise/">national</a> poker champion.</p>
+	    	<p>Previously at a tech consulting firm in Singapore, I led the Consumer Technologies division, specialising in B2C Engagement and Loyalty. I advised clients on product strategy, and built a team of developers and designers to execute digital products.</p>
+	    	<p>Prior to that, I founded and ran a startup in Vietnam. We had a good run, pivoted a few times, but eventually failed.</p>
+	    	<p>I like to challenge myself in competitive strategy games such as Poker and Settlers of Catan - I'm a <a href="http://somuchpoker.com/lee-yin-how-wins-the-record-breaking-asia-poker-league-vietnam-main-event/">regional</a> and <a href="http://www.pokerkaki.com/article/the_singapore_poker_championships_ii_-_poker_in_paradise/">national</a> poker champion.</p>
 			<Img
 		      fluid={data.apl.childImageSharp.fluid}
 		      alt="winning asia poker league"
-		    />	   	    			    
+		    />
+		    <p style={{fontSize: "0.7em", fontStyle: "italic"}}>vanity photo</p>	   	    			    
 	    	<h3>About this site</h3>
 	    	<p>Built with Gatsby and Netlify</p>
 	    	<p>Credit: dark theme toggle and implementation from Dan Abramov</p>
