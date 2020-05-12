3d55ddd01f5362a63fc8ecf693ea732aa687f023
yinhow <yinhowlew@gmail.com>
2020-05-11 20:59:08 -0400

    update metatag

--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
+          <meta name="google-site-verification" content="VP9tJQDiQdTzVnb4WjeXzodHDlnGOd3WaYSv6oJ1t0w" />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes} className="light">
