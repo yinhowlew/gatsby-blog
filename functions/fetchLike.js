3b377e6e5c987f18f22449ae8f580a19c143bbff
yinhow <yinhowlew@gmail.com>
2020-05-13 12:57:03 -0400

    testing netlify functions15

--- functions/fetchLike.js
+++ functions/fetchLike.js
@@ -8,27 +8,21 @@ const firebaseConfig = {
   projectId: "yinhow-blog",
 };
 
-exports.handler = async function(event, context, callback) {
+exports.handler = function(event, context, callback) {
 
 	firebase.initializeApp(firebaseConfig);
 	firebase.firestore();
 	// const original = post.fields.slug;
     // const slug = original.substring(1, original.length-1);
     const slug = event.queryStringParameters.slug
-    console.log("slug", slug)
-    // var count = 0;
 
-    const count = await firebase.firestore().collection('post').doc(slug).get()
+    const count = firebase.firestore().collection('post').doc(slug).get()
     .then(function(post) {
     	// console.log("post", post.data())
     	return post.data().count
     })
-    // try RETURN and a variable
-    console.log(count)
-    // console.log("count", count)
- //    const count = post.data().count
- 	// try .then and fill a variable w data
-	// console.log("count", count)
+    console.log(count) // this fetches the right data
+    // but somehow the end point 
 
     const response = {
     	statusCode: 200,
