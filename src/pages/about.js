c31fb705ec2b5ab63a68e46d25d9b1d229337d8a
yinhow <yinhowlew@gmail.com>
2020-06-01 12:52:21 -0400

    update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import { graphql, Link } from "gatsby";
 // import Header from "../components/header";
 // import styles from "../styles/about.module.css";
 import Layout from "../components/Layout";
-import Img from "gatsby-image";
+// import Img from "gatsby-image";
 import SEO from '../components/SEO';
 // import Heart from '../components/Heart';
 
@@ -20,12 +20,14 @@ const About = ({ data }) => {
 	    	<p>My professional journey converges on building things - tech products, startups, teams, companies.</p> 
 	    	<p>Previously at a tech consulting firm in Singapore, I led the Consumer Technologies division, specialising in B2C Engagement and Loyalty. I advised clients on product strategy, and built a team of developers and designers to execute digital products.</p>
 	    	<p>Prior to that, I founded and ran a startup in Vietnam. We had a good run, pivoted a few times, but eventually failed.</p>
-	    	<p>I like to challenge myself in competitive strategy games such as Poker and Settlers of Catan - I'm a <a href="http://somuchpoker.com/lee-yin-how-wins-the-record-breaking-asia-poker-league-vietnam-main-event/">regional</a> and <a href="http://www.pokerkaki.com/article/the_singapore_poker_championships_ii_-_poker_in_paradise/">national</a> poker champion.</p>
+	    	<p>Outside work, I like to challenge myself in competitive strategy games - I'm a <a href="http://somuchpoker.com/lee-yin-how-wins-the-record-breaking-asia-poker-league-vietnam-main-event/">regional</a> and <a href="http://www.pokerkaki.com/article/the_singapore_poker_championships_ii_-_poker_in_paradise/">national</a> poker champion.</p>
+{/*
 			<Img
 		      fluid={data.apl.childImageSharp.fluid}
 		      alt="winning asia poker league"
 		    />
 		    <p style={{fontSize: "0.7em", fontStyle: "italic"}}>vanity photo</p>	   	    			    
+*/}	    	
 	    	<h3>About this site</h3>
 	    	<p>Built with Gatsby and Netlify</p>
 	    	<p>Credit: dark theme toggle and implementation from Dan Abramov</p>
