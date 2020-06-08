c5b6a670b3644acb765d8d02b4a9bc1355537401
yinhow <yinhowlew@gmail.com>
2020-06-08 15:35:35 -0400

    update env variable2

--- functions/serverlessConfig.js
+++ functions/serverlessConfig.js
@@ -2,7 +2,7 @@ var firebase = require("firebase/app")
 require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvJNmJXsqCSmvOOej6omsjoQhr1yGou_M",
+  apiKey: process.env.firebase_apiKey,
   databaseURL: "https://yinhow-blog.firebaseio.com", 
   storageBucket: 'yinhow-blog.appspot.com', 
   projectId: "yinhow-blog",
